6b963e9f622fc0a2e42481c1977fa6eec82c3bac
akummerfeldt <kumm.eak@gmail.com>
2024-04-01 12:49:48 -0500

    Updating

--- script.js
+++ script.js
@@ -1,29 +1,31 @@
 let p = document.querySelector(".quote-p");
+const currentLocation = window.location.href;
 
-
-
-//
-document.addEventListener('keydown', function(event) {
-    if (event.ctrlKey && event.shiftKey && event.key === 'x') {
-        // Your function here
-        console.log('Ctrl+Shift+X pressed');
-    } 
-    if (event.metaKey && event.shiftKey && event.key === 'x') {
-        // Your function here
-        console.log('Command+Shift+X pressed');
-        
-    }
-});
 
 
 function selectedText(){
- // Get the selection object
- let selection = window.getSelection();
+    // Get the selection object
+    let selection = window.getSelection();
+   
+    // Get the selected text as a string
+    let selectedText = selection.toString();
+   
+    // Log the selected text to the console
+   
+    p.value = selectedText;
+    alert(selectedText)
+   }
+   
+//
+window.document.addEventListener('keydown', function(event) {
+    if ( event.shiftKey) {
+       alert('Ctrl+Shift+X pressed');
+    selectedText()
+    } 
+    if (event.metaKey && event.shiftKey && event.key === 'x') {
+        console.log('Command+Shift+X pressed');
+        // Call another function here if needed
+    }
+});
 
- // Get the selected text as a string
- let selectedText = selection.toString();
-
- // Log the selected text to the console
- alert(selectedText)
-}
 
